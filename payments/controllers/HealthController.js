6edfacbbd2062515bc6e15d0939b0c89792a689d
Phil Evans <pevans@paylocity.com>
2024-02-07 21:58:38 -0500

    healthcontroller changes

--- payments/controllers/HealthController.js
+++ payments/controllers/HealthController.js
@@ -9,13 +9,14 @@ class HealthController{
         this.requestCount++; 
         this.logger.info(`Liveness request count ${this.requestCount}`);
 
-        if (this.requestCount > 5){
-            res.status(500);
-            res.send();
-            return;
-        }
-
         res.status(200); 
+        res.send();
+    }
+
+    async handleReadiness(req, res){
+        this.logger.info('Readiness request handled');
+
+        res.status(200);
         res.send();
     }
 }
@@ -27,6 +28,10 @@ module.exports = (logger) => {
 
     router.get('/liveness', function(req, res){
         controller.handleLiveness(req, res);
+    })
+
+    router.get('/readiness', function(req, res){
+        controller.handleReadiness(req, res);
     })
 
     return router;
